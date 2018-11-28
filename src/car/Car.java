5810337e61552dfb3ff644c48d626723d19d7957
Angelina Deniaga <deniagaangelina@gmail.com>
2018-11-28 00:34:23 -0500

    initial commit

---
+++ src/car/Car.java
@@ -0,0 +1,92 @@
+package car;
+
+
+import javafx.scene.paint.Color;
+
+import java.time.LocalDate;
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author BesoAngel
+ */
+
+public class Car {
+
+    private Color color;
+
+    private String model;
+
+    private int yearOfManufacture;
+
+    private int horsePower;
+
+    public Color getColor() {
+        return color;
+    }
+
+    public void setColor(Color color) {
+        if (color == null)
+            throw new IllegalArgumentException("Color can't be null");
+        this.color = color;
+    }
+
+    public String getModel() {
+        return model;
+    }
+
+    public void setModel(String model) {
+        if (model == null)
+            throw new IllegalArgumentException("Model can't be null");
+        if (model.trim().length() == 0)
+            throw new IllegalArgumentException("Model can't be empty");
+        this.model = model.trim();
+    }
+
+    public int getYearOfManufacture() {
+        return yearOfManufacture;
+    }
+
+    public void setYearOfManufacture(int yearOfManufacture) {
+        if (yearOfManufacture <= 1930)
+            throw new IllegalArgumentException("The car is too old");
+        else if (yearOfManufacture > LocalDate.now().getYear())
+            throw new IllegalArgumentException("The year of manufacture can't be in a future");
+
+        this.yearOfManufacture = yearOfManufacture;
+    }
+
+    public int getHorsePower() {
+        return horsePower;
+    }
+
+    public void setHorsePower(int horsePower) {
+
+        if (horsePower <= 0)
+            throw new IllegalArgumentException("the horse power value is too low");
+        else if (horsePower >= 1000)
+            throw new IllegalArgumentException("the horse power value is too high");
+
+        this.horsePower = horsePower;
+    }
+
+    public Car(Color color, String model, int yearOfManufacture, int horsePower) {
+        setColor(color);
+        setModel(model);
+        setYearOfManufacture(yearOfManufacture);
+        setHorsePower(horsePower);
+    }
+
+    @Override
+    public String toString() {
+        return getModel() + ", manufactured in " + getYearOfManufacture() + ", " + getHorsePower() + " hp";
+    }
+}
+
+
+
